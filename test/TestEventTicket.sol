bbfc0d89bd4c1c2cba510e925d0331ff8a34f60c
Alkim Gozen <alkimake@gmail.com>
2019-06-03 22:09:59 +0900

    solidity test for buyTickets (not working)

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -5,15 +5,42 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/EventTickets.sol";
 import "../contracts/EventTicketsV2.sol";
 
+// Proxy contract for testing throws
+contract ThrowProxy {
+  address public target;
+  bytes data;
+
+  constructor(address _target) internal {
+    target = _target;
+  }
+
+  //prime the data using the fallback function.
+  function() external {
+    data = msg.data;
+  }
+
+  function execute() external returns (bool) {
+    (bool r, ) = target.call(data);
+	return r;
+  }
+}
+
 contract TestEventTicket {
+
+	uint public initialBalance = 1 ether;
 
 	string description = "description";
 	string url = "URL";
 	uint ticketNumber = 100;
 	EventTickets myEvent;
+    uint ticketPrice = 100 wei;
 
 	function beforeEach() public {
 		myEvent = new EventTickets(description, url, ticketNumber);
+	}
+
+	function testSelf() public {
+		Assert.equal(address(this).balance, 1 ether, 'not enough balance to test');
 	}
 
 	function testSetup()
@@ -29,6 +56,12 @@ contract TestEventTicket {
 		Assert.equal(website, url, "the event urls should match");
 		Assert.equal(totalTickets, ticketNumber, "the number of tickets for sale should be set");
 		Assert.equal(sales, 0, "the ticket sales should be 0");
+	}
+
+	function testBuyTickets() public {
+		myEvent.buyTickets.value(ticketPrice)(1);
+		(, , , uint sales, ) = myEvent.readEvent();
+		Assert.equal(sales, 1, 'the ticket sales should be 1');
 	}
 
 }
