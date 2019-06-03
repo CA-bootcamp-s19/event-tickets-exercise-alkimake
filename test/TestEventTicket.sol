b500cead925a846939782cc74b26d160e68eb6a5
Alkim Gozen <alkimake@gmail.com>
2019-06-03 17:31:35 +0900

    first tests are added

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -7,6 +7,22 @@ import "../contracts/EventTicketsV2.sol";
 
 contract TestEventTicket {
 
+	string description = "description";
+	string url = "URL";
+	uint ticketNumber = 100;
+	address account = DeployedAddresses.EventTickets();
+	EventTickets myEvent;
 
+	function beforeEach() public {
+		myEvent = new EventTickets(description, url, ticketNumber);
+	}
+
+	function testSetup()
+		public
+	{
+		Assert.equal(myEvent.owner(), address(this), 'the deploying address should be the owner');
+		(, , , , bool isOpen) = myEvent.readEvent();
+		Assert.equal(isOpen, true, 'the event should be open');
+	}
 
 }
